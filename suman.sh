ba78527fcc75e12b46e92f9b25230ff8b6f14603
sumanto-23 <64468496+sumanto-23@users.noreply.github.com>
2022-09-18 12:17:45 +0530

    Update suman.sh

--- suman.sh
+++ suman.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
+docker pull httpd
 docker run -dit --name hello1  httpd
-docker cp work.sh hello1:/usr
+docker cp ./work.sh hello1:/usr
+docker ps
 docker exec hello1 bash -c "/usr/work.sh"
 
